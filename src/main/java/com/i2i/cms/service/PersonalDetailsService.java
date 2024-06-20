b088d9a443cb79f03b42608dfae267efa78f6eb7
sivageetha-murugan <sivageetha.murugan@ideas2it.com>
2024-06-20 15:06:19 +0530

    Deleted maven- hibernate source code

--- src/main/java/com/i2i/cms/service/PersonalDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.i2i.cms.service;
-
-import com.i2i.cms.dao.PersonalDetailsDao;
-
-public class PersonalDetailsService {
-
-    private PersonalDetailsDao personalDetailsDao = new PersonalDetailsDao();
-
-}
